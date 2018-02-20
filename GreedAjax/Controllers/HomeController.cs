611293f1725b49b35b57112d66b270b8b0149379
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-02-20 18:28:59 +0900

    初回コミット

---
+++ GreedAjax/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreedAjax.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Child()
+        {
+            var model = new ChildModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Child(ChildModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // 成功
+                var result = new ContentResult() { Content = "Success" };
+                return result;
+            }
+            
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+    public class ChildModel
+    {
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ GreedAjax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GreedAjax.MvcApplication" Language="C#" %>
